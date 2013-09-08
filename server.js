ca7521b0fc99314092a61c2b45b3af2fd2135657
Kushagra Gour <kushagra@wingify.com>
2013-09-09 04:03:38 +0530

    serve index.html

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-var app = require('http').createServer(),
+var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
     fs = require('fs');
 
 io.configure(function(){
       io.set('transports', ['xhr-polling']);
 });
+
+
+function handler (req, res) {
+      fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+                if (err) {
+                          res.writeHead(500);
+                                return res.end('Error loading index.html');
+                                    }
+
+                                        res.writeHead(200);
+                                            res.end(data);
+                                              });
+}
+
 
 var players = [],
     updateInterval = 800,
